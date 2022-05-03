e3b90189aa5d5f730a4ca0b579d09ad59b2e07b2
viragloriya <viragloriya1996@gmail.com>
2022-05-04 00:37:51 +0530

    Revert "minor changes"
    This reverts commit 38eb918bbdc898e6cc7f83b7b4742f2abf380325.

--- src/main/java/com/malachi/backend/controller/HomeController.java
+++ src/main/java/com/malachi/backend/controller/HomeController.java
@@ -4,8 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("home")
@@ -21,8 +20,4 @@ public class HomeController {
         companyList.add("Tux & Lux");
         return companyList;
     }
-
-
-
-
 }
